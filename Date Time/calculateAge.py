8c6f434b90b553eac49650b4c181179e49444c15
Gauttam Sonkamble <gauttamsonkamble358@gmail.com>
2025-10-03 20:38:10 +0530

    updated

---
+++ Date Time/calculateAge.py
@@ -0,0 +1,14 @@
+
+from datetime import date
+
+def age(dob):
+    today = date.today()
+
+    years = today.year - dob.year
+
+    if (today.month, today.day) < (dob.month,dob.day):
+        years -= 1
+
+    return years 
+
+print('Age : ',age(date(1996, 6, 3)))
---
+++ Date Time/stringToObject.py
@@ -0,0 +1,10 @@
+
+import datetime
+
+str_date = input('Enter a date in DD-MM-YYYY format: ')
+
+d, m, y = str_date.split('-')
+
+d1 = datetime.date(int(y),int(m),int(d))
+
+print('Date : ',d1)
