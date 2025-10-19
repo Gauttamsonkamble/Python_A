dd3832c67e98fdda90c474c56172dcb3a2b90bb7
Gauttam Sonkamble <gauttamsonkamble358@gmail.com>
2025-10-19 11:06:09 +0530

    inserting into student table

--- SQL Practice/insertingToStudentTable.py
+++ SQL Practice/insertingToStudentTable.py
@@ -5,11 +5,16 @@ conn = sqlite3.connect('univ.db')
 
 cursor = conn.cursor()
 
+# rollNo = int(input("Enter Roll No: "))
+# name = input("Enter Name : ")
+# city = input("Enter City : ")
+# deptno = int(input("Enter Department No: "))
+
 rollNo = int(input("Enter Roll No: "))
 name = input("Enter Name : ")
 city = input("Enter City : ")
 deptno = int(input("Enter Department No: "))
-
+    
 cursor.execute(f"insert into students values({rollNo},'{name}','{city}',{deptno})")
 
 conn.commit()
